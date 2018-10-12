0374dab7ab673aa2b3124ad1f3a43386ef0375cf
HarshavardhanAnne <harshanne1996@gmail.com>
2018-10-11 20:35:34 -0400

    Added comments on how to use

--- program.sh
+++ program.sh
@@ -1,5 +1,17 @@
 #! /bin/bash
 
+###HOW TO###
+# sudo chmod +x program.sh
+
+# Method 1:
+#	./program.sh
+#	This method uses the default directory name you are currently in.
+
+# Method 2:
+#	./program.sh filename.elf
+#	This method uses a specified filename from the user.
+############
+
 elfname="${PWD##*/}.elf"
 if [ "$1" != "" ]; then
 	elfname=$1
