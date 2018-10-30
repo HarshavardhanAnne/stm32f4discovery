c888b5d651bbfcd7b16bd0487ac96dfc11f9d2f9
HarshavardhanAnne <harshanne1996@gmail.com>
2018-10-29 20:37:23 -0400

    H

--- rtos_stm32f401/program.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-elfname="${PWD##*/}.elf"
-if [ "$1" != "" ]; then
-	elfname=$1
-fi
-
-openocd -f board/stm32f4discovery.cfg -f my_stm32f4.cfg -c "myFlash $elfname"
--- stm32f401c_disco/build/program.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-openocd -f board/stm32f4discovery.cfg -f my_stm32f4.cfg -c "myFlash stm32f401c_disco.elf"
