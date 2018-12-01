abf72c9338d7f4978a17834804fbe3fce08fcf68
HarshavardhanAnne <harshanne1996@gmail.com>
2018-12-01 18:56:50 -0500

    Flashing elf instead of binary

---
+++ eecs473_solarcar/program.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+###HOW TO###
+# sudo chmod +x program.sh
+
+# Method 1:
+#	./program.sh
+#	This method uses the default directory name you are currently in.
+
+# Method 2:
+#	./program.sh filename.elf
+#	This method uses a specified filename from the user.
+############
+
+elfname="${PWD##*/}.elf"
+binname="${PWD##*/}.bin"
+if [ "$1" != "" ]; then
+	elfname=$1
+fi
+
+openocd -f board/stm32f4discovery.cfg -f my_stm32f4.cfg -c "myFlash $elfname" #replace binname with elfname
